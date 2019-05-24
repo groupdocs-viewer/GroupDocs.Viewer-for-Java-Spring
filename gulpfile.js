7328d01acc366dace0abfd82cd26845a09e3dabe
Elena Kolesnikova <koleslena@yandex.ru>
2019-05-24 22:17:17 +0600

    redone build for new angular ui

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require('gulp')
 
 gulp.task('build', function() {
   return gulp
-    .src('./node_modules/@groupdocs.examples.jquery/**')
-    .pipe(gulp.dest('./src/main/resources/static/assets/'))
+    .src('./node_modules/@groupdocs.total-angular/viewer/dist/**')
+    .pipe(gulp.dest('./src/main/resources/static/'))
 })
