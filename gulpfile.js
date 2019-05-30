16c9dc42010f7eb7c2ca8ac38833b4de5fc2850d
Elena Kolesnikova <koleslena@yandex.ru>
2019-05-30 17:32:23 +0600

    fixed build

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,12 @@ var gulp = require('gulp')
 
 gulp.task('build', function() {
   return gulp
-    .src('./node_modules/@groupdocs.examples.angular/viewer/dist/**')
-    .pipe(gulp.dest('./src/main/resources/static/'))
-})
+      .src('./node_modules/@groupdocs.examples.angular/**')
+      .pipe(gulp.dest('./src/main/resources/static/angular/'))
+});
+
+gulp.task('config', function() {
+  return gulp
+      .src('./node_modules/@groupdocs.examples.angular/viewer/dist/assets/config/**')
+      .pipe(gulp.dest('./src/main/resources/static/assets/config/'))
+});
