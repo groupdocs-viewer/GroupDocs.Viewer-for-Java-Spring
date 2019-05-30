d0977b97d7ce1b75322ae43e031ad548a706af93
Elena Kolesnikova <koleslena@yandex.ru>
2019-05-30 13:04:16 +0600

    fixed viewer angular package

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require('gulp')
 
 gulp.task('build', function() {
   return gulp
-    .src('./node_modules/@groupdocs.total-angular/viewer/dist/**')
+    .src('./node_modules/@groupdocs.examples.angular/viewer/dist/**')
     .pipe(gulp.dest('./src/main/resources/static/'))
 })
