3cccdf234df76b8cbcf63067f13755d0ea4374cf
Elena Kolesnikova <koleslena@yandex.ru>
2019-10-24 19:59:53 +0600

    issue #46 increased version of viewer ui, fixed scroll

--- client/apps/viewer/src/app/custom-viewer/custom-viewer.component.ts
+++ client/apps/viewer/src/app/custom-viewer/custom-viewer.component.ts
@@ -1,6 +1,16 @@
-import { Component } from '@angular/core';
-import { ViewerAppComponent, ViewerConfigService, ViewerService } from '@groupdocs.examples.angular/viewer';
-import { ModalService, UploadFilesService, NavigateService, ZoomService, PagePreloadService, RenderPrintService, PasswordService, WindowService } from '@groupdocs.examples.angular/common-components';
+import {Component} from '@angular/core';
+import {ViewerAppComponent, ViewerConfigService, ViewerService} from '@groupdocs.examples.angular/viewer';
+import {
+  ModalService,
+  UploadFilesService,
+  NavigateService,
+  ZoomService,
+  PagePreloadService,
+  RenderPrintService,
+  PasswordService,
+  WindowService,
+  LoadingMaskService
+} from '@groupdocs.examples.angular/common-components';
 
 @Component({
   selector: 'client-custom-viewer',
@@ -8,9 +18,11 @@ import { ModalService, UploadFilesService, NavigateService, ZoomService, PagePre
   styleUrls: ['./custom-viewer.component.css']
 })
 export class CustomViewerComponent extends ViewerAppComponent {
-    public loading;
-    constructor(_viewerService: ViewerService, _modalService: ModalService, configService: ViewerConfigService, uploadFilesService: UploadFilesService, _navigateService: NavigateService, _zoomService: ZoomService, pagePreloadService: PagePreloadService, _renderPrintService: RenderPrintService, passwordService: PasswordService, _windowService: WindowService) {
-    super(_viewerService,_modalService,configService,uploadFilesService,_navigateService,_zoomService,pagePreloadService,_renderPrintService,passwordService,_windowService);
+  public loading;
+
+  constructor(_viewerService: ViewerService, _modalService: ModalService, configService: ViewerConfigService, uploadFilesService: UploadFilesService, _navigateService: NavigateService, _zoomService: ZoomService, pagePreloadService: PagePreloadService, _renderPrintService: RenderPrintService, passwordService: PasswordService, _windowService: WindowService,
+              _loadingMaskService: LoadingMaskService) {
+    super(_viewerService, _modalService, configService, uploadFilesService, _navigateService, _zoomService, pagePreloadService, _renderPrintService, passwordService, _windowService, _loadingMaskService);
 
     /**
      *  Put your document retrieval logic here
@@ -21,12 +33,12 @@ export class CustomViewerComponent extends ViewerAppComponent {
      */
     const params = new URLSearchParams(window.location.search);
     const url = params.get('url');
-    if(url){
+    if (url) {
       const filename = url.split('/').pop();
       this.loading = true;
       _viewerService.upload(null, url, false).subscribe(() => {
         this.loading = false;
-        this.selectFile(filename,'','');
+        this.selectFile(filename, '', '');
       });
     }
   }
